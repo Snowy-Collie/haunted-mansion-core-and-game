03c83ac70aa4abc118b282f58bc288e53be9c266
snowy_collie <kzalexcheng@icloud.com>
2026-06-04 13:11:59 -0700

    Change code through PEP 8 and License update

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
-# Python3
-import sys, os, glob, json, urllib.request
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# ==============================================================================
+# MIT License
+#
+# Copyright (c) 2026 Snowy Collie
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+import glob
+import json
+import os
+import sys
+import urllib.request
 def step(choice):
     global text
     print("\033[36m"+text[choice][0])
@@ -39,7 +69,7 @@ def load():
             with urllib.request.urlopen(req) as response:
                 with open("./games/pilot_game_en.json", 'wb') as f:
                     f.write(response.read())
-        except:
+        except Exception:
             pass
     
     filelst=glob.glob("./games/*.json")
